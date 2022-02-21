dc74306e992dbf171ec028437db49cdf528e7cf9
Dramane <Dramane.dev@gmail.com>
2022-02-21 14:57:24 +0100

    Dev link list added

---
+++ src/pages/movies.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import DisplayMovies from '../Components/Design-list/DisplayMovies';
+
+const Movies = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
+    >
+      <DisplayMovies />
+    </div>
+  );
+};
+
+export default Movies;
